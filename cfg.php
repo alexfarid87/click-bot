054b925e2a5800082e77f50e9ec8cda5bd96c656
alexfarid87 <69816614+alexfarid87@users.noreply.github.com>
2020-09-17 02:57:40 +0800

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,15 @@
+<?php
+// -*- Copyleft @ °✍⎯⚚͜͡SʰⁱNᵒᵇⁱ - 2020 -*-
+// -*- Config Bitly -*-
+// -*- If you want to donate, at DOGE Wallet:
+// -*- DDsu3tCfqXbhoj1j84Ydq3qkBYNZZESC3Q
+//________________________________________________
+
+// Website https://bit.ly/3m2njZk
+// Input your data in here || Tulis data kamu di bawah
+
+$userAgent = "Mozilla/5.0 (Linux; Android 7.1.2; Redmi Note 5A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36";
+$PHPSESSID = "SesHashKey=5wnvhwc0302mmwhm; SesToken=ses_id%3D3272%26ses_key%3D5wnvhwc0302mmwhm; AccExist=3272; PHPSESSID=5d8ev953e9orbder1isopn9ev3";
+
+
+?>
